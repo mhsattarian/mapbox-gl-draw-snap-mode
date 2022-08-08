c0562521aba0c2316db8b6bb47fde99575607b2d
Jozef Balun <jozef.balun@pixwell.sk>
2022-08-08 21:46:59 +0200

    add direct select snapping

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { default as SnapPointMode } from "./modes/snap_point.js";
 export { default as SnapLineMode } from "./modes/snap_line.js";
 export { default as SnapPolygonMode } from "./modes/snap_polygon.js";
+export { default as SnapDirectSelect } from "./modes/snap_direct_select.js";
 
 export { default as SnapModeDrawStyles } from "./utils/customDrawStyles.js";
---
+++ src/modes/snap_direct_select.js
@@ -0,0 +1,85 @@
+import * as Constants from "@mapbox/mapbox-gl-draw/src/constants";
+import doubleClickZoom from "@mapbox/mapbox-gl-draw/src/lib/double_click_zoom";
+import DirectSelect from "@mapbox/mapbox-gl-draw/src/modes/direct_select";
+import { createSnapList, getGuideFeature, IDS, snap } from "./../utils";
+
+const SnapDirectSelect = { ...DirectSelect };
+
+SnapDirectSelect.onSetup = function (opts) {
+  const featureId = opts.featureId;
+  const feature = this.getFeature(featureId);
+
+  if (!feature) {
+    throw new Error("You must provide a featureId to enter direct_select mode");
+  }
+
+  if (feature.type === Constants.geojsonTypes.POINT) {
+    throw new TypeError("direct_select mode doesn't handle point features");
+  }
+
+  const [snapList, vertices] = createSnapList(this.map, this._ctx.api, feature);
+
+  const verticalGuide = this.newFeature(getGuideFeature(IDS.VERTICAL_GUIDE));
+  const horizontalGuide = this.newFeature(
+    getGuideFeature(IDS.HORIZONTAL_GUIDE)
+  );
+
+  this.addFeature(verticalGuide);
+  this.addFeature(horizontalGuide);
+
+  const state = {
+    map: this.map,
+    featureId,
+    feature,
+    dragMoveLocation: opts.startPos || null,
+    dragMoving: false,
+    canDragMove: false,
+    selectedCoordPaths: opts.coordPath ? [opts.coordPath] : [],
+    vertices,
+    snapList,
+    verticalGuide,
+    horizontalGuide,
+  };
+
+  state.options = this._ctx.options;
+
+  this.setSelectedCoordinates(
+    this.pathsToCoordinates(featureId, state.selectedCoordPaths)
+  );
+  this.setSelected(featureId);
+  doubleClickZoom.disable(this);
+
+  this.setActionableState({
+    trash: true,
+  });
+
+  const optionsChangedCallBAck = (options) => {
+    state.options = options;
+  };
+
+  // for removing listener later on close
+  state["optionsChangedCallBAck"] = optionsChangedCallBAck;
+  this.map.on("draw.snap.options_changed", optionsChangedCallBAck);
+
+  return state;
+};
+
+SnapDirectSelect.dragVertex = function (state, e, delta) {
+  const { lng, lat } = snap(state, e);
+
+  state.feature.updateCoordinate(state.selectedCoordPaths[0], lng, lat);
+};
+
+SnapDirectSelect.onStop = function (state) {
+  this.deleteFeature(IDS.VERTICAL_GUIDE, { silent: true });
+  this.deleteFeature(IDS.HORIZONTAL_GUIDE, { silent: true });
+
+  // remove moveemd callback
+  //   this.map.off("moveend", state.moveendCallback);
+  this.map.off("draw.snap.options_changed", state.optionsChangedCallBAck);
+
+  // This relies on the the state of SnapPolygonMode being similar to DrawPolygon
+  DirectSelect.onStop.call(this, state);
+};
+
+export default SnapDirectSelect;
